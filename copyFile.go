01a95786faf0ca48625aa66b6b1c612d06ceb64a
ardeshir sepahsalar <ardeshir.org@gmail.com>
2019-11-17 16:15:37 -0600

    removed space

--- copyFile.go
+++ copyFile.go
@@ -1,147 +1,144 @@
 package main
 
 import (
-   // "fmt"
-   "io"
-   "flag"
-   "os"
-   "log"
-   u "github.com/ardeshir/version"
+	// "fmt"
+	"flag"
+	"io"
+	"log"
+	"os"
+
+	u "github.com/ardeshir/version"
 )
 
-var ( 
- debug bool = false
- version string = "0.0.0"
- )
+var (
+	debug   bool   = false
+	version string = "0.0.0"
+)
 
 func main() {
 
-    filename    := flag.String("file", defaultFile(), "Name of 1st file to use with createFile() ")
-    text        := flag.String("text", "This default text will be printed\n", "Some text goes here")
-    newfilename := flag.String("newfile", defaultFile2(), "Name of 2nd file use with createFile() ")
-    flag.Parse()
-    
-     deleteFile(*filename)
-     // delete file used the checkExistance before deleting.
-     // checkExistence(*filename) 
-     createFile(*filename)
-     // deleteFile(*filename)
-     writeToFile(*filename, *text)
-     // renameFile(*filename, "newfiletest.txt")
-     copyFile(*filename, *newfilename)
-     renameFile(*newfilename, "newfiletest2.txt")
-     // deleteFile(*newfilename)
-     // writeToFile(*filename, *text)
-      
-  if debugTrue() {
-    u.V(version)
-  }
+	filename := flag.String("file", defaultFile(), "Name of 1st file to use with createFile() ")
+	text := flag.String("text", "This default text will be printed\n", "Some text goes here")
+	newfilename := flag.String("newfile", defaultFile2(), "Name of 2nd file use with createFile() ")
+	flag.Parse()
+
+	deleteFile(*filename)
+	// delete file used the checkExistance before deleting.
+	// checkExistence(*filename)
+	createFile(*filename)
+	// deleteFile(*filename)
+	writeToFile(*filename, *text)
+	// renameFile(*filename, "newfiletest.txt")
+	copyFile(*filename, *newfilename)
+	renameFile(*newfilename, "newfiletest2.txt")
+	// deleteFile(*newfilename)
+	// writeToFile(*filename, *text)
+
+	if debugTrue() {
+		u.V(version)
+	}
 
 }
 func writeToFile(filename string, text string) {
-    f, err := os.Create(filename)
-    u.ErrNil(err, "Can't create new file")
-    
-    defer f.Close()
-    
-    if _, err := f.Write([]byte(text)); err != nil {
-        log.Fatalln(err)
-    }
-    
-    log.Println("Done\n")
+	f, err := os.Create(filename)
+	u.ErrNil(err, "Can't create new file")
+
+	defer f.Close()
+
+	if _, err := f.Write([]byte(text)); err != nil {
+		log.Fatalln(err)
+	}
+
+	log.Println("Done\n")
 }
 
 // Function to copy a file
 func copyFile(filename string, newfilename string) {
-    of, err := os.Open(filename)
-    u.ErrNil(err, "Can't Open old file")
-    defer of.Close()
-    
-    nf, err2 := os.Create(newfilename)
-    u.ErrNil(err2, "Can't create new file")
-    defer nf.Close()
-    
-    bw, err3 := io.Copy(nf, of)
-    u.ErrNil(err3, "Can't copy from old to new")
-    log.Printf("Bytes written: %d\n", bw)
-    
-    if err4 := nf.Sync(); err4 != nil {
-        log.Fatalln(err4)
-    }
-    log.Printf("Done copying from %s to %s\n", filename, newfilename)
+	of, err := os.Open(filename)
+	u.ErrNil(err, "Can't Open old file")
+	defer of.Close()
+
+	nf, err2 := os.Create(newfilename)
+	u.ErrNil(err2, "Can't create new file")
+	defer nf.Close()
+
+	bw, err3 := io.Copy(nf, of)
+	u.ErrNil(err3, "Can't copy from old to new")
+	log.Printf("Bytes written: %d\n", bw)
+
+	if err4 := nf.Sync(); err4 != nil {
+		log.Fatalln(err4)
+	}
+	log.Printf("Done copying from %s to %s\n", filename, newfilename)
 
 }
-
 
 // Function to rename a file
-func renameFile ( filename string, newname string) {
-        checkExistence(filename)
-        err := os.Rename(filename, newname)
-        u.ErrNil(err, "File was corrupted")
-        
-    fi, err2 := os.Stat(newname)
-    if err2 != nil {
-        if os.IsNotExist(err2) {
-           log.Fatalf("File: %s does not exist", fi.Name)
-        } 
-    } 
-    
-    log.Printf("Exists, last modified %v\n", fi.ModTime())
-    // deleteFile(newname)
+func renameFile(filename string, newname string) {
+	checkExistence(filename)
+	err := os.Rename(filename, newname)
+	u.ErrNil(err, "File was corrupted")
+
+	fi, err2 := os.Stat(newname)
+	if err2 != nil {
+		if os.IsNotExist(err2) {
+			log.Fatalf("File: %s does not exist", fi.Name)
+		}
+	}
+
+	log.Printf("Exists, last modified %v\n", fi.ModTime())
+	// deleteFile(newname)
 }
 
-
-// Function to create a file 
-func createFile( filename string) {
-    f, err := os.Create(filename)
-    u.ErrNil(err, "Unable to create file")
-    defer f.Close()
-    log.Printf("Created %s\n", f.Name())
+// Function to create a file
+func createFile(filename string) {
+	f, err := os.Create(filename)
+	u.ErrNil(err, "Unable to create file")
+	defer f.Close()
+	log.Printf("Created %s\n", f.Name())
 }
 
-// Function to delete a file 
-func deleteFile( filename string) {
-     // createFile()  // use the createFile first, then delete file
-     checkExistence(filename)
-     err := os.Remove(filename)
-     u.ErrNil(err, "Unable to remove testfile1.txt")
-     log.Printf("Deleted %s", filename)
+// Function to delete a file
+func deleteFile(filename string) {
+	// createFile()  // use the createFile first, then delete file
+	checkExistence(filename)
+	err := os.Remove(filename)
+	u.ErrNil(err, "Unable to remove testfile1.txt")
+	log.Printf("Deleted %s", filename)
 }
 
-func checkExistence( filename string) {
-    // createFile()
-    fi, err := os.Stat(filename)
-    if err != nil {
-        if os.IsNotExist(err) {
-           log.Fatalf("File: %s does not exist", fi.Name)
-        } 
-    } 
-    
-    log.Printf("Exists, last modified %v\n", fi.ModTime())
-    // deleteFile()
-}
+func checkExistence(filename string) {
+	// createFile()
+	fi, err := os.Stat(filename)
+	if err != nil {
+		if os.IsNotExist(err) {
+			log.Fatalf("File: %s does not exist", fi.Name)
+		}
+	}
 
+	log.Printf("Exists, last modified %v\n", fi.ModTime())
+	// deleteFile()
+}
 
 // Function to check env variable DEFAULT_DEBUG bool
 func debugTrue() bool {
-    
-     if os.Getenv("DEFAULT_DEBUG") != "" {
-        return true
-     }  
-     return false 
+	if os.Getenv("DEFAULT_DEBUG") != "" {
+		return true
+	}
+	return false
 }
 
 // Function to check env variable DEFAULT_FILE to get
 func defaultFile() string {
-    if os.Getenv("DEFAULT_FILE") != "" {
-        return os.Getenv("DEFAULT_FILE")
-    }
-    return "testfile.txt"
+	if os.Getenv("DEFAULT_FILE") != "" {
+		return os.Getenv("DEFAULT_FILE")
+	}
+	return "testfile.txt"
 }
 
 func defaultFile2() string {
-    if os.Getenv("DEFAULT_FILE2") != "" {
-        return os.Getenv("DEFAULT_FILE2")
-    }
-    return "newtestfile.txt"
+	if os.Getenv("DEFAULT_FILE2") != "" {
+		return os.Getenv("DEFAULT_FILE2")
+	}
+	return "newtestfile.txt"
 }
